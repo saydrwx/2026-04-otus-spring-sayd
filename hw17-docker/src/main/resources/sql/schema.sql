884c4f07ff210f37ea761cf69ca82c3fc79c7905
Seymur Aidynov <sn.aidynov@nspk.ru>
2026-09-03 16:16:54 +0300

    add hw17

---
+++ hw17-docker/src/main/resources/sql/schema.sql
@@ -0,0 +1,31 @@
+create table authors (
+    id bigserial,
+    full_name varchar(255) not null,
+    primary key (id)
+);
+
+create table genres (
+    id bigserial,
+    name varchar(255) not null,
+    primary key (id)
+);
+
+create table books (
+    id bigserial,
+    title varchar(255) not null,
+    author_id bigint references authors (id) on delete cascade,
+    primary key (id)
+);
+
+create table books_genres (
+    book_id bigint references books(id) on delete cascade,
+    genre_id bigint references genres(id) on delete cascade,
+    primary key (book_id, genre_id)
+);
+
+create table comments (
+    id bigserial,
+    text varchar(1000) not null,
+    book_id bigint references books(id) on delete cascade,
+    primary key (id)
+)
